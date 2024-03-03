1544ba3227411ccb99c3ec15bc058c1721f3154c
Davron <tdavron@gmail.com>
2024-03-03 13:44:26 +0500

    q

--- src/views/ComponentView.vue
+++ src/views/ComponentView.vue
@@ -1,17 +1,10 @@
 <template>
   <div class="home">
-   <h3>Component page</h3>
+    <h3>Component page</h3>
+    <Component1>1</Component1>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
+<script setup>
 import Component1 from '@/components/Component1.vue'
-
-export default {
-  name: 'HomeView',
-   components: {
-    Component1
-   }
-}
 </script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,14 +4,6 @@
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-//import HelloWorld from '@/components/HelloWorld.vue'
+<script  setup>
 
-export default {
-  name: 'HomeView',
-  // components: {
-  //   HelloWorld
-  // }
-}
 </script>
