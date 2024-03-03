58ae0c4597afc14505af8bc10aee4313461da074
Davron <tdavron@gmail.com>
2024-03-03 13:06:35 +0500

    1

---
+++ src/views/ComponentView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+   <h3>Component page</h3>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Component1 from '@/components/Component1.vue'
+
+export default {
+  name: 'HomeView',
+   components: {
+    Component1
+   }
+}
+</script>
