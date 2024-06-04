4fc48b7d21ac77da501b250eab1727c89ffab52a
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-06-04 12:26:44 +0500

    wge

--- src/views/jasurMusicumAsosiy.vue
+++ src/views/jasurMusicumAsosiy.vue
@@ -3,36 +3,39 @@
         <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
                 <button class="btn btn-outline-darck" style="height: 50px; width: 50px; background-color: brown; color: white;"><</button>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                                data-bs-toggle="dropdown" aria-expanded="false">
-                                Home
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Action</a></li>
-                                <li><a class="dropdown-item" href="#">Another action</a></li>
-                                <li>
-                                    <hr class="dropdown-divider">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li class="nav-item">
                                 </li>
-                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                <div class="btn-group dropend">
+                                    <button type="button" class="btn btn-secondary dropdown-toggle"
+                                        data-bs-toggle="dropdown" aria-expanded="false" style="background-color: white; color: black; border: none; width: 80px;">Home
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li>
+                                            <hr class="dropdown-divider">
+                                        </li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </div>
+                                <li class="nav-item">
+                                    <a class="nav-link active" aria-current="page" href="#">Popular Artist </a>
+                                </li>
                             </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">Popular Artist </a>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" style="width: 318px;" placeholder="Search" aria-label="Search">
-                        <button class="btn btn-outline-darck" type="submit" style="height: 50px; width: 50px;"></button>
-                    </form>
-                </div>
+                            <form class="d-flex">
+                                <input class="form-control me-2" type="search" style="width: 318px;"
+                                    placeholder="Search" aria-label="Search">
+                                <button class="btn btn-outline-darck" type="submit"
+                                    style="height: 50px; width: 50px;"></button>
+                            </form>
+                        </div>
             </div>
         </nav>
     </div>
