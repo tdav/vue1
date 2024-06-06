18d85832f8893868df9a8119a65c96c7cd7925c3
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-06-06 18:42:50 +0500

    qww

--- src/components/MusicBottom.vue
+++ src/components/MusicBottom.vue
@@ -10,12 +10,15 @@
                     <p style="font-size: 14px; display: inline; color: white">Shape of You</p>
                     <p style="font-size: 14px; color: white;">Ed Sheeran</p>
                 </div>
-
-                <svg style=" color: white;" mlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                    class="bi bi-heart mb-3" viewBox="0 0 16 16">
-                    <path
-                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-                </svg>
+                <p class="m-0">
+                    <button id="btnh1" class="btn mb-4">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                            class="bi bi-heart-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
+                        </svg>
+                    </button>
+                </p>
             </div>
             <div class="col-6 pt-4" style="height: 100vh; color: white; text-align: center">
                 <!-- <> -->
@@ -116,6 +119,21 @@
 </script>
 
 <style scoped>
+#btnh1 {
+    width: 100%;
+    padding: 0px;
+    text-align: center;
+    color: white
+}
+
+#btnh1:hover {
+    color: red;
+}
+
+#btnh1:active {
+    border: none;
+}
+
 .range::-webkit-slider-thumb {
     -webkit-appearance: none;
     box-shadow: -100% 0 0 100% white;
