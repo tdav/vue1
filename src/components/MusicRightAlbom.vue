076d455d15f21550c71b7cde8c610e50537b3c13
azizbek5999 <azizbek.isakboyev@gmail.com>
2024-06-29 16:36:49 +0500

    udgare

--- src/components/MusicRightAlbom.vue
+++ src/components/MusicRightAlbom.vue
@@ -3,82 +3,8 @@
       <div class="iwuc mt-4">
         <p>Recent Played</p>
         <a class="uwhiu" style="text-decoration: none">See All</a>
-      </div>
-      <!-- <div class="row music-play" style="height: 60px">
-        <img
-          class="col-1"
-          style="height: 45px; width: 65px"
-          src="../assets/img/Rectangle1.png"
-          alt=""
-        />
-        <div class="col-8 p-0 m-0 text-center">
-          <p class="m-0 p-0 owd">Perfect</p>
-          
-          <p class="m-0 mt-3 ms-3 owdg ">Ed Sheeran</p>
-        </div>
-         -->
-      <!-- </div>
-      <div class="row music-play" style="height: 60px">
-        <img
-          class="col-1"
-          style="height: 45px; width: 65px"
-          src="../assets/img/Rectangle2.png"
-          alt=""
-        />
-        <div class="col-8 p-0 m-0 text-center">
-          <p class="m-0 p-0 me-4 owd">Picisan</p>
-          
-          <p class="m-0 m mt-3 me-4 owdg ">Hanin</p>
-        </div>
-        
-       
-      </div> -->
-      <!-- <div class="row music-play" style="height: 60px">
-        <img
-          class="col-1"
-          style="height: 45px; width: 65px"
-          src="../assets/img/Rectangle3.png"
-          alt=""
-        />
-        <div class="col-8 p-0 m-0 text-center">
-          <p class="m-0 p-0 me-4 owd">Title</p>
-          
-          <p class="m-0 m mt-3 me-4 owdg ">Meghan</p>
-        </div>
-       
-       
-      </div> -->
-      <!-- <div class="row music-play" style="height: 60px">
-        <img
-          class="col-1"
-          style="height: 45px; width: 65px"
-          src="../assets/img/Rectangle5.png"
-          alt=""
-        />
-        <div class="col-8 p-0 m-0 text-center">
-        <p class="m-0 p-0 me-2 owd">{{ props.item.name }}</p>
-          <p class="m-0 m mt-3 me-2 owdg ">Ed Sheeran</p>
-        </div>
-       
-        
-      </div>
-      <div class="row music-play" style="height: 60px">
-        <img
-          class="col-1"
-          style="height: 45px; width: 65px"
-          src="../assets/img/Rectangle4.png"
-          alt=""
-         <
-        <div class="col-8 p-0 m-0 text-center">
-          <p class="m-0 p-0 me-2 owd">Perfect</p>
-          
-          <p class="m-0 m mt-3 me-2 owdg ">Ed Sheeran</p>
-        </div>
-       
-        
-      </div> -->
-      
-      <MusicRightComponents />
+      </div> 
+      <MusicRightComponents class="mb-3" />
     </div>
 </template>
 
