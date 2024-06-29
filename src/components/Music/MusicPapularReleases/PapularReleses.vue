200b6474af37d7848809c01ff5f44b327ba476ae
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-06-29 15:13:16 +0500

    ihg

--- src/components/Music/MusicPapularReleases/PapularReleses.vue
+++ src/components/Music/MusicPapularReleases/PapularReleses.vue
@@ -1,32 +1,9 @@
 <template>
-    <div
-        v-for="it in popularReleasesMusic"
-        :key="it.id"
-        class="col-2"
-        style="text-align: center;"
-    >
-        <img
-            :src="it.imageUrl"
-            alt=""
-            class="_img"
-        >
+    <div v-for="it in PopularReleasesMusic" :key="it.id" class="col-2" style="text-align: center;">  
+        <img :src="it.imageUrl" alt="" class="kdscb">
         <p>{{ it.name }}</p>
     </div>
 </template>
-<script setup>
-import { onBeforeMount, reactive } from 'vue';
-import { replaceArray, getAssetImg } from '@/plugins/services';
-const popularReleasesMusic = reactive([])
-onBeforeMount(async () => {
-    const _popularReleasesMusic = await fetch("../../../assets/jsonData/popular-releases-music.json").then(res => res?.json())
-    replaceArray(popularReleasesMusic, _popularReleasesMusic)
-})
+<script>
+import PopularReleasesMusic from '../../../assets/jsonData/PopularReleasesMusic.json'
 </script>
-
-
-<style lang="scss" scoped>
-._img {
-    width: 100%;
-    object-fit: cover;
-}
-</style>
