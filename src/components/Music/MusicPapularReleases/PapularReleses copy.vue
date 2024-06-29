05bae6afbb65be9124c738d111d5377e2210b5d9
Davron <tdavron@gmail.com>
2024-06-29 15:05:21 +0500

    11

---
+++ src/components/Music/MusicPapularReleases/PapularReleses copy.vue
@@ -0,0 +1,11 @@
+<!-- <template>
+    <div v-for="it in PopularReleasesMusic" :key="it.id" class="col-2" style="text-align: center;">  
+        <img :src="it.imageUrl" alt="" class="kdscb">
+        <p>{{ it.name }}</p>
+    </div>
+    
+</template>
+
+<script setup>
+import PopularReleasesMusic from '../../../assets/jsonData/PopularReleasesMusic.json'
+</script> -->
