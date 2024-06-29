86c350181defaf948ff0c09ef79d7bde445204e7
azizbek5999 <azizbek.isakboyev@gmail.com>
2024-06-29 14:26:56 +0500

    uyt

--- src/components/Music/MusicComponents/MusicRightComponents.vue
+++ src/components/Music/MusicComponents/MusicRightComponents.vue
@@ -7,7 +7,7 @@
           alt=""
         >
         <div class="col-8 p-0 m-0 text-center">
-          <p class="m-0 p-0 me-2" style="color: white; float: left;">{{ it.albomName }}</p>
+          <p class="m-0 p-0 me-2" style="color: white; float: left; font-size: 10px;">{{ it.albomName }}</p>
           
           <p class="m-0 m mt-3 me-2" style="color: white; float: right;">{{ it.actor }}</p>
         </div>
