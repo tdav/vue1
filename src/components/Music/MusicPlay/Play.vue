8f8ec109c2921748b32cd674f53be669c8d634f9
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-06-29 10:26:36 +0500

    wsw

--- src/components/Music/MusicPlay/Play.vue
+++ src/components/Music/MusicPlay/Play.vue
@@ -10,7 +10,7 @@
                 <button id="btnh1" class="btn mb-4" @click="menuSelect(1)"
                     :class="[menuSelectActiveIndex == 1 ? 'menuSelectActive' : '']">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                        class="bi bi-heart-fill" viewBox="0 0 16 16">
+                        class="bi bi-heart-fill lekfnnok" viewBox="0 0 16 16">
                         <path fill-rule="evenodd"
                             d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314" />
                     </svg>
@@ -38,17 +38,19 @@
                         d="M.404 7.304a.802.802 0 0 0 0 1.392l6.363 3.692c.52.302 1.233-.043 1.233-.696V4.308c0-.653-.713-.998-1.233-.696z" />
                 </svg>
                 <!-- < -->
-                <svg v-if="isPlaying" xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor"
-                    class="bi bi-pause-circle-fill ms-4 jabx" viewBox="0 0 16 16" @click="toggleIcons">
-                    <path
-                        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
-                </svg>
+                <div style="display: inline-block; " class="p-0 ms-4 pt-1 m-0 iug">
+                    <svg v-if="isPlaying" xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor"
+                        class="bi bi-pause-circle-fill jabx" viewBox="0 0 16 16" @click="toggleIcons">
+                        <path
+                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0M6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5m3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5" />
+                    </svg>
 
-                <svg v-else xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor"
-                    class="bi bi-caret-right-fill ms-4 jabx" viewBox="0 0 16 16" @click="toggleIcons">
-                    <path
-                        d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" width="33" height="33" fill="currentColor"
+                        class="bi bi-caret-right-fill jabx" viewBox="0 0 16 16" @click="toggleIcons">
+                        <path
+                            d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
+                    </svg>
+                </div>
                 <!-- > -->
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
                     class="bi bi-fast-forward-fill ms-4 eovi" viewBox="0 0 16 16">
@@ -60,7 +62,7 @@
                 <!-- > -->
                 <!-- repet -->
                 <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" fill="currentColor"
-                    class="bi bi-repeat ms-4" viewBox="0 0 16 16">
+                    class="bi bi-repeat ms-4 odwi" viewBox="0 0 16 16">
                     <path
                         d="M11 5.466V4H5a4 4 0 0 0-3.584 5.777.5.5 0 1 1-.896.446A5 5 0 0 1 5 3h6V1.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384l-2.36 1.966a.25.25 0 0 1-.41-.192m3.81.086a.5.5 0 0 1 .67.225A5 5 0 0 1 11 13H5v1.466a.25.25 0 0 1-.41.192l-2.36-1.966a.25.25 0 0 1 0-.384l2.36-1.966a.25.25 0 0 1 .41.192V12h6a4 4 0 0 0 3.585-5.777.5.5 0 0 1 .225-.67Z" />
                 </svg>
@@ -92,8 +94,28 @@
     </div>
 </template>
 <style scoped>
+.iug:hover {
+    transition: all 1s ;
+    display: inline-block;
+    border: 1px solid;
+    width: 40px;
+    height: 40px;
+    border-radius: 50px;
+    border-color: rgb(105, 105, 255);
+}
+
+.odwi:hover {
+    transition: all 3s;
+    rotate: 360deg;
+}
+
+.lekfnnok:hover {
+    color: red;
+    transition: all 1s;
+}
+
 .menuSelectActive {
-    color: red; 
+    color: red;
 }
 
 .hiuiu {
@@ -111,15 +133,6 @@
     text-align: center;
     color: white;
 }
-
-/* #btnh1:hover {
-    color: red;
-} */
-
-/* #btnh1:active {
-    border: none;
-    color: red;
-} */
 
 .jabx:active {
     color: rgb(66, 66, 255);
@@ -139,9 +152,9 @@ function toggleIcons() {
 }
 
 
-import { Ref } from 'vue';  
+import { Ref } from 'vue';
 
-let menuSelectActiveIndex = ref(1); 
+let menuSelectActiveIndex = ref(1);
 
 function menuSelect(index) {
     menuSelectActiveIndex = index;
