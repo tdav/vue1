bccc40ad57d03f439ee9cf4030f08a5aaf11737b
azizbek5999 <azizbek.isakboyev@gmail.com>
2024-06-29 14:21:05 +0500

    i876

--- src/components/Music/MusicComponents/MusicRightComponents.vue
+++ src/components/Music/MusicComponents/MusicRightComponents.vue
@@ -3,16 +3,16 @@
         <img
           class="col-1"
           style="height: 45px; width: 65px"
-          :src="require(it.imageUrl)"
+          :src="it.imgUrl"
           alt=""
         >
-        <img :src="require(it.imageUrl)" alt="" class="kdscb">
         <div class="col-8 p-0 m-0 text-center">
           <p class="m-0 p-0 me-2 owd">{{ it.albomName }}</p>
+          
           <p class="m-0 m mt-3 me-2 owdg ">{{ it.actor }}</p>
         </div>
       </div>
 </template>
 <script setup>
-import albomlist from '../../../assets/jsonData/albomlist.json'
+import albomlist from "../../../assets/jsonData/albomlist.json"
 </script>
