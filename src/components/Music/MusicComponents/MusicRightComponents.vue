e3d518422f1c6d74f8b74a63a490e4319beb89ac
azizbek5999 <azizbek.isakboyev@gmail.com>
2024-06-29 14:23:55 +0500

    kj

--- src/components/Music/MusicComponents/MusicRightComponents.vue
+++ src/components/Music/MusicComponents/MusicRightComponents.vue
@@ -7,18 +7,12 @@
           alt=""
         >
         <div class="col-8 p-0 m-0 text-center">
-          <p class="m-0 p-0 me-2 owd">{{ it.albomName }}</p>
+          <p class="m-0 p-0 me-2" style="color: white;">{{ it.albomName }}</p>
           
-          <p class="m-0 m mt-3 me-2 owdg ">{{ it.actor }}</p>
+          <p class="m-0 m mt-3 me-2" style="color: white;">{{ it.actor }}</p>
         </div>
       </div>
 </template>
 <script setup>
 import albomlist from "../../../assets/jsonData/albomlist.json"
 </script>
-<style scoped>
-.owdg {
-  color: #ffffff7c;
-  font-size: 12px;
-}
-</style>
