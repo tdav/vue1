c72baa1b1740d25f0328229d4782d3d1a2eb9f7c
43x5c6fvg7 <omonshakarbekov41@gmail.com>
2024-07-01 23:59:52 +0300

    1

--- src/views/Music.vue
+++ src/views/Music.vue
@@ -11,10 +11,9 @@
                             </div>
                             <button class="btn btn-outline-darck ms-4" type="submit"  @click="toggleOffcanvas"
                                 style="height: 50px; width: 50px; background-color: #303030; border-radius:  15px;">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" style="color: white;"
-                                    fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd"
-                                        d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0" />
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16" style="color:#FFFFFF">
+                                    <path fill-rule="evenodd" 
+                                        d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
                                 </svg>
                             </button>
 
