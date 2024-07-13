9fd93db70ce5ab9b2b5eb0e52127d0aeaf2de16e
43x5c6fvg7 <omonshakarbekov41@gmail.com>
2024-07-13 15:13:29 +0300

    1234

--- src/views/Omon/todo.vue
+++ src/views/Omon/todo.vue
@@ -1,78 +1,84 @@
 <template>
     <div class="container">
-  
-      <div class="row">
-        <div class="col">
-          <input type="text" class="textstyle" v-model="msg">
-          <button @click="OnCreate" class="buttonstyle">Qo'shish</button>
+        <div class="row">
+            <div class="col">
+                <input type="text" class="sdf" v-model="msg">
+                <button @click="OnCreate" class="iuj">Qo'shish</button>
+            </div>
         </div>
-      </div>
-  
-  
-      <div class="row">
-        <div class="col">
-          <h3>Todo List</h3>
-          <ul class="list-group" v-for="it in list" :key="it.id">
-            <li>
-              <input type="checkbox" v-model="it.checked"/>
-              <p class="mytext"> {{ it.name }}</p>
-            </li>
-          </ul>
 
-          <ul class="list-group" v-for="it in list" :key="it.id">
-            <li>
-              <p class="mytext"> {{ it.name }}</p>
-            </li>
-          </ul>
-
+        <div class="row">
+            <div class="col">
+                <h3>Todo List</h3>
+                <ul class="list-group">
+                    <li v-for="it in list" :key="it.id">
+                        <input type="checkbox" v-model="it.checked" @change="handleCheck(it)" />
+                        <p class="mytext ms-2"> {{ it.name }}</p>
+                    </li>
+                </ul>
+            </div>
         </div>
-      </div>
+        <div class="row">
+           
+            <h3 class="mt-4">Bajarilgan</h3>
+            <div class="col">
+                <ul class="list-group">
+                    <li v-for="it in donelist" :key="it.id">
+                        <input type="checkbox" v-model="it.checked" @change="handleCheck(it)" />
+                        <p class="mytext ms-2"> {{ it.name }}</p>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const list = ref([])
-  
-  let msg = ref("")
-  
-  
-  function OnCreate() {
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const list = ref([]);
+const donelist = ref([]);
+
+let msg = ref("");
+
+function OnCreate() {
     var it = {
-      checked: false,
-      name: msg
+        id: Date.now(),
+        checked: false,
+        name: msg.value
+    };
+
+    msg.value = "";
+
+    list.value.push(it);
+}
+
+function handleCheck(item) {
+    if (item.checked) {
+        list.value = list.value.filter(it => it.id !== item.id);
+        donelist.value.push(item);
+    } else {
+        donelist.value = donelist.value.filter(it => it.id !== item.id);
+        list.value.push(item);
     }
-  
-    msg = ref("")
-  
-    list.value.push(it)
-  }
-  
-    
-  
-  </script>
-  
-  <style scoped>
-  .mytext {
+}
+</script>
+
+<style scoped>
+.mytext {
     font-size: medium;
     font-weight: 500;
-    padding-left: 6px;
-  }
-
-.buttonstyle {
-    margin-left: 2rem;
-    border: 2px solid;
-    border-radius: 10px;
-    border-color: black;
-    color: black;
-    background-color: grey;
-    
 }
 
-.textstyle {
-    width: 15rem;
+.iuj {
+    background: grey;
+    border: 2px solid black;
+    color: black;
+    margin-left: 2rem;
+    border-radius: 10px;
+}
+.sdf {
+    width: 17rem;
     height: 2rem;
 }
-  </style>
-  
+</style>
