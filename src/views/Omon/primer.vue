d2dfb7bd5dc8a97e507d8a5475f40a9cc7df264a
43x5c6fvg7 <omonshakarbekov41@gmail.com>
2024-07-15 10:46:55 +0300

    1234

---
+++ src/views/Omon/primer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <div v-for="(item, index) in items" :key="index">
+        <input type="checkbox" v-model="checkedItems[index]">
+        <label>{{ item }}</label>
+        <button @click="removeItem(index)">O'chirish</button>
+      </div>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const items = ref(['Item 1', 'Item 2', 'Item 3']);
+  const checkedItems = ref([false, false, false]);
+  
+  const removeItem = (index) => {
+    items.value.splice(index, 1);
+    checkedItems.value.splice(index, 1);
+  };
+  </script>
+  
