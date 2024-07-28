7c030c4a4ca6da73c74d3ab51a231cf0c0621e51
Davron <tdavron@gmail.com>
2024-07-28 15:46:01 +0500

    11

--- src/views/davron/todo.vue
+++ src/views/davron/todo.vue
@@ -14,13 +14,13 @@
       <div class="col">
         <h3>Todo List</h3>
         <ul class="list-group" v-for="it in list" :key="it.id">
-          
-          
+
+
           <li>
             <input type="checkbox" v-model="it.checked" />
             <p class="mytext"> {{ it.name }}</p>
           </li>
-        </ul> 
+        </ul>
       </div>
     </div>
   </div>
@@ -46,6 +46,77 @@ function OnCreate() {
   list.value.push(it)
 }
 
+
+let g1 = 1
+let g2 = 2
+let g3 = 3
+
+
+g3 += g2;
+
+g3 = g3 + g2;
+
+
+function TestSum1(g1, g2, g3) {
+  return g1 + g2 + g3;
+}
+
+let o1 = {
+  g1: 1,
+  g2: 1,
+  g3: 3
+}
+
+function TestSun2(o1) {
+  return o1.g1 + o1.g2 + o1.g2
+}
+
+
+let ggg2 = o1.g2;
+let ggg1 = ol['g2'];
+
+
+
+let str = g2 == g1 ? 'true' : 'false'
+
+if (g2 === g1)
+  console.log("true");
+else
+  console.log("fasle");
+
+switch (g1) {
+  case 1:
+    console.log("fasle");
+    break;
+  case 2:
+    console.log("fasle 1");
+    break;
+  case 3:
+    console.log("fasle");
+    break;
+
+  default:
+    break;
+}
+
+
+console.log(`if (g2 == 1)
+  console.log("true");
+else
+  if (g2 == 2)
+    console.log("true");
+  else
+    if (g2 == 3)
+      console.log("true");`)
+
+if (g2 == 1)
+  console.log("true");
+else
+  if (g2 == 2)
+    console.log("true");
+  else
+    if (g2 == 3)
+      console.log("true");
 
 
 
