644ab46c25e7a28d5d97623a3feb1d73ac00403b
azizbek5999 <azizbek.isakboyev@gmail.com>
2024-07-28 18:33:43 +0500

    azizbek adgare

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Music from '../views/Music.vue'
 import OmonTodo from '../views/Omon/todo.vue'
 import Aziz from '../views/Azizbek/Aziz.vue'
 import Operatorlar from '../views/Omon/operatorlar.vue'
+import newOperators from '@/views/Azizbek/newOperators.vue'
 import Sikllar from '../views/Omon/sikllar.vue'
 
 const routes = [
@@ -37,6 +38,12 @@ const routes = [
     path: '/aziz',
     name: 'Aziz',
     component: Aziz
+  },
+
+  {
+    path: '/newOperators',
+    name: 'newOperators',
+    component: newOperators
   },
   
   {
