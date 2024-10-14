642037362243b9b34194840b01e4bd6d065ea19d
43x5c6fvg7 <omonshakarbekov41@gmail.com>
2024-10-14 15:55:01 +0300

    123

--- src/views/Davron/myStore/comm2.vue
+++ src/views/Davron/myStore/comm2.vue
@@ -1,20 +1,18 @@
 <template>
-    <div>
-       <h3>COM2</h3>
-        <input :value="store.state.keyWord" @input="changeKeyWord" />
-        <br>
-    </div>
+  <div>
+    <input :value="store.state.keyWord" @input="changeKeyWord" />
+  </div>
 </template>
 
-<script setup>
-import { useStore } from 'vuex'
-const store = useStore()
-
-
-function changeKeyWord(evt) {
-    this.store.changeKeyWord(evt.target.value);
-}
-
+<script>
+export default {
+  inject: ["store"],
+  methods: {
+    changeKeyWord(evt) {
+      this.store.changeKeyWord(evt.target.value);
+    },
+  },
+};
 </script>
 
 <style></style>
--- src/views/Davron/myStore/index.vue
+++ src/views/Davron/myStore/index.vue
@@ -1,25 +1,18 @@
 <template>
-
-    <comp1 />
-    <comp2 />
-
-    <div>
-        Store Ichidan:<br /> {{ keyWord==null }}
-    </div>
-
+  <div>
+    <input :value="store.state.keyWord" @input="changeKeyWord" />
+  </div>
 </template>
 
-<script setup>
-import comp1 from "./comm1.vue";
-import comp2 from "./comm2.vue";
-
-import { computed } from 'vue'
-import { useStore } from 'vuex'
-const store = useStore()
-
-
-const keyWord = computed(()=>store.getters.keyWord)
-//const size = computed(() => store.getters.GET_SIZE);
-
+<script>
+export default {
+  inject: ["store"],
+  methods: {
+    changeKeyWord(evt) {
+      this.store.changeKeyWord(evt.target.value);
+    },
+  },
+};
 </script>
- 
+
+<style></style>
