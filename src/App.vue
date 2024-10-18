ad24276d610b40b40079de6927044027bb60fd42
kasimovjasur75 <kasimovjasur75@gmail.com>
2024-10-18 21:51:38 +0500

    m

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div v-if="path != '/Music'" style="height: 100vh">
     <div class="d-flex" id="wrapper">
       <div v-if="path != '/Music'" class="border-end" id="sidebar-wrapper">
-        <div class="sidebar-heading border-bottom my-menu-color">
+        <div class="sidebar-heading border-bottom my-menu-color ps-2 p-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="currentColor"
@@ -559,6 +559,9 @@ const toggle1 = () => {
 
 .accordion-button.collapsed::after {
   background-image: url("data:image/svg+xml,%3csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%23fff'%3e%3cpath fill-rule='evenodd' d='M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z'/%3e%3c/svg%3e");
+}
+.border-end{
+  background-color: #414751;
 }
 
 .accordion-button:not(.collapsed) {
